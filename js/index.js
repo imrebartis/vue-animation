95bfc288ccdb2fbf6c569f34bf8c550da9f0df74
Imre Bartis <imre.bartis@siili.com>
2018-01-25 09:02:42 +0200

    JS animation

--- js/index.js
+++ js/index.js
@@ -1,22 +1,61 @@
-var Child = {
-  template: '#childarea',
-  props: ['isShowing'],
-  methods: {
-    toggleShow: function toggleShow() {
-      this.isShowing = !this.isShowing;
-    }
-  }
-};
-
-new Vue({
+var app6 = new Vue({
   el: '#app',
   data: function data() {
     return {
-      isShowing: false
+      message: 'This is a good place to type things.',
+      load: false
     };
   },
 
-  components: {
-    appChild: Child
+  methods: {
+    // All hooks pass in the el parameter 
+    // (short for element) except on the actual animation hooks, 
+    // enter and leave, which also pass done as a parameter, which
+    //  is used to tell Vue that the animation is completed.
+    beforeEnter: function beforeEnter(el) {
+      TweenMax.set(el, {
+        transformPerspective: 600,
+        perspective: 300,
+        transformStyle: "preserve-3d",
+        autoAlpha: 1
+      });
+    },
+    enter: function enter(el, done) {
+      var loader = this.load;
+      var split = new SplitText(el, { type: "words" }),
+          wordCount = split.words.length,
+          tl = new TimelineMax({
+        onComplete: done
+      });
+
+      for (var i = 0; i < wordCount; i++) {
+        TweenMax.set(split.words[i], {
+          x: Math.floor(Math.random() * (1 + 300 - -300) + -300)
+        });
+      }
+
+      tl.add("drop");
+      for (var i = 0; i < wordCount; i++) {
+        tl.from(split.words[i], 1.5, {
+          z: Math.floor(Math.random() * (1 + 150 - -150) + -150),
+          ease: Bounce.easeOut
+        }, "drop+=0." + [i] / 0.5);
+
+        tl.fromTo(split.words[i], 1.5, {
+          y: -800
+        }, {
+          y: 150,
+          ease: Bounce.easeOut
+        }, "drop+=0." + [i] / 0.5);
+
+        tl.to(split.words[i], 0.5, {
+          rotation: 720,
+          y: 0,
+          x: 0,
+          z: 0,
+          ease: Sine.easeIn
+        }, "drop+=1." + (5 + [i] / 0.5));
+      }
+    }
   }
 });
